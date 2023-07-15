050b68a6618c3553b7106b190d83d592561c9d84
fbanquart <fbanquart@acssi.fr>
2023-07-15 12:42:06 +0200

    npm run build

--- back/index.js
+++ back/index.js
@@ -13,9 +13,9 @@ let cards = require("./data/cards");
 app.use('/api/cards', helperRouteJsonDal.createDalRoute('cards', cards))
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(__dirname + '/public/'));
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
+app.use(express.static(__dirname + '/public/'));
+app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
